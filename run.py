029a6a97970984185a871a3380d535c58bc445e6
Maya-Claveau <maya.claveau@gmail.com>
2023-10-15 15:51:32 +0000

    Add run.py

--- run.py
+++ run.py
@@ -1,3 +1,24 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+""" password generator """
+import random
+
+
+def shuffle(string):
+    """ shuffle the password function"""
+    tempList = list(string)
+    random.shuffle(tempList)
+    return "".join(tempList)
+
+
+uppercaseLetter1 = chr(random.randint(65, 90))
+uppercaseLetter2 = chr(random.randint(65, 90))
+lowercaseLetter1 = chr(random.randint(97, 122))
+lowercaseLetter2 = chr(random.randint(97, 122))
+digit1 = chr(random.randint(48, 57))
+digit2 = chr(random.randint(48, 57))
+punctuationSign1 = chr(random.randint(33, 64))
+punctuationSign2 = chr(random.randint(91, 96))
+
+password = uppercaseLetter1 + uppercaseLetter2 + lowercaseLetter1 + lowercaseLetter2 + digit1 + digit2 + punctuationSign1 + punctuationSign2
+newPassword = shuffle(password)
+
+print(newPassword)
